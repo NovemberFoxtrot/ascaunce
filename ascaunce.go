65a3facfa5368d4d92a4452df29fc8082b8de325
November Foxtrot <november@fox.trot>
2013-08-13 00:45:02 +0900

    multi fun

--- ascaunce.go
+++ ascaunce.go
@@ -6,37 +6,96 @@ import (
 	"io"
 	"os"
 	"runtime"
+	"strings"
 )
 
 type textdata struct {
-	data []string
+	rawdata []string
+	data    []string
 }
 
 func (t *textdata) input(f io.Reader) {
 	scanner := bufio.NewScanner(f)
 
 	if t.data == nil {
-		t.data = make([]string, 0)
+		t.rawdata = make([]string, 0)
 	}
 
 	// todo scan options
 	for scanner.Scan() {
 		someline := scanner.Text()
-		t.data = append(t.data, someline)
+		t.rawdata = append(t.rawdata, someline)
 	}
 }
 
-func (t *textdata) parse() {
-	fmt.Println(len(t.data))
+func (t *textdata) tf(term string, norm bool) float64 {
+	return 0.0
 }
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
+func parse(lines []string) <-chan string {
+	out := make(chan string, 8*100)
+
+	go func() {
+		for _, line := range lines {
+			if len(line) > 0 {
+			  out <- line
+			}
+		}
+
+		close(out)
+	}()
+
+	return out
+}
+
+func parselower(in <-chan string) <-chan string {
+	out := make(chan string, cap(in))
+
+	go func() {
+		for line := range in {
+			out <- strings.ToLower(line)
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func parsesplit(in <-chan string) <-chan string {
+	out := make(chan string, cap(in))
+
+	go func() {
+		for line := range in {
+			grams := strings.Split(line, " ")
+			for _, gram := range grams {
+				if len(gram) > 0 {
+				  out <- gram
+				}
+			}
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func resulter(in <-chan string) {
+	for gram := range in {
+		fmt.Println(gram, len(gram))
+	}
+}
+
 func main() {
 	t := new(textdata)
 
 	t.input(os.Stdin)
-	t.parse()
+
+	parser := parse(t.rawdata)
+	parselower := parselower(parser)
+	parsespliter := parsesplit(parselower)
+	resulter(parsespliter)
 }
